47a44ec91a0c60f65cdbfed2b3ced347cc6c19a6
Rushat gupta chadha <rushatgc@gmail.com>
2016-09-17 13:43:50 -0400

    automated graph generation

--- hw1.py
+++ hw1.py
@@ -5,17 +5,22 @@ class Learner():
 	def __init__(self, algorithm, label_gen, experts):
 		if algorithm == "WMA":
 			self.algorithm = 0
+			self.algorithm_name = algorithm
 		elif algorithm == "RWMA":
 			self.algorithm = 1
+			self.algorithm_name = algorithm
 		else:
 			print "check algorithm spelling"
 
 		if label_gen == "stochastic":
 			self.labels = 0
+			self.label_name = label_gen
 		elif label_gen == "deterministic":
 			self.labels = 1
+			self.label_name = label_gen
 		elif label_gen == "adversarial":
 			self.labels = 2
+			self.label_name = label_gen
 		else:
 			print "check label generator spelling"
 
@@ -47,7 +52,7 @@ class Learner():
 		return y_hat
 
 	def true_label(self, h):
-		if self.labels ==0:
+		if self.labels == 0:
 			y  = choice([-1,1])
 		elif self.labels ==1:
 			#always winning 
@@ -72,8 +77,9 @@ class Learner():
 				ind = np.argmax(np.random.multinomial(1, np.divide(self.weights,phi))) 
 			y_hat = self.predictor(hypothesis,ind)
 			y = self.true_label(hypothesis)
-			#print "yh", y_hat
-			#print "yo", y
+			print "y_hat", y_hat
+			print "expert 3 prediction",self.expertAdvice(i)[2]
+			print "y_actual", y
 			loss = self.calculateLoss(y,y_hat)
 			self.loss.append(loss)
 			for ind in range(self.experts):
@@ -82,8 +88,9 @@ class Learner():
 				regret = (np.sum(self.loss) - np.sum(self.expert_loss[ind]))/(i+1.)
 				self.regret[ind].append(regret)
 			self.weights = self.updateWeights(hypothesis,y,eta)
-			#print "loss", loss
-			#print "weights", self.weights
+			print "loss", loss
+			print "expert3 loss", self.expert_loss[2][-1]
+			print "weights", self.weights
 		print "Total loss", np.sum(self.loss)
 
 	def calculateLoss(self, y,y_hat):
@@ -104,44 +111,25 @@ class Learner():
 	def plotLossGraph(self,T):
 		time = np.arange(T)
 		f, axarr = plt.subplots(3, sharex=True)
-		axarr[0].set_title('Loss Plot')
-		axarr[0].plot(time,self.loss, label='Total loss')
-		axarr[0].plot(time,self.expert_loss[0], label='Expert1')
-		axarr[0].set_ylim([-0.5, 1.5])
-		axarr[1].plot(time,self.loss, label='Total loss')
-		axarr[1].plot(time,self.expert_loss[1], label='Expert2')
-		axarr[1].set_ylim([-0.5, 1.5])
-		axarr[2].plot(time,self.loss, label='Total loss')
-		axarr[2].plot(time,self.expert_loss[2], label='Expert3')
-		axarr[2].set_ylim([-0.5, 1.5])
+		for i in range(self.experts):
+			axarr[i].set_title('Loss Plot for {} and {} case'.format(self.algorithm_name, self.label_name))
+			axarr[i].plot(time,self.loss, label='Algorithm loss')
+			axarr[i].plot(time,self.expert_loss[i], label='Expert {}'.format(i))
+			axarr[i].set_ylim([-0.5, 1.5])
+			axarr[i].legend()
+			axarr[i].set_ylabel('loss')
 		axarr[2].set_xlabel('time')
-		axarr[0].set_ylabel('loss')
-		axarr[1].set_ylabel('loss')
-		axarr[2].set_ylabel('loss')
-		axarr[0].legend()
-		axarr[1].legend()
-		axarr[2].legend()
-
+		
 	def plotRegretGraph(self,T):
 		time = np.arange(T)
 		f, axarr = plt.subplots(3, sharex=True)
-		axarr[0].set_title('Average Regret Plot')
-		axarr[0].plot(time,self.regret[0], label='Expert1')
-		#axarr[0].set_ylim([-0.5, 1.5])
-		axarr[1].plot(time,self.regret[1], label='Expert2')
-		#axarr[1].set_ylim([-0.5, 1.5])
-		axarr[2].plot(time,self.regret[2], label='Expert3')
-		#axarr[2].set_ylim([-0.5, 1.5])
-		
+		for i in range(self.experts):
+			axarr[i].set_title('Average Regret Plot for {} and {} case'.format(self.algorithm_name, self.label_name))
+			axarr[i].plot(time,self.regret[i], label='Expert{}'.format(i))
+			axarr[i].set_ylabel('avg regret')
+			axarr[i].legend()
 		axarr[2].set_xlabel('time')
-		axarr[0].set_ylabel('avg regret')
-		axarr[1].set_ylabel('avg regret')
-		axarr[2].set_ylabel('avg regret')
 		
-		axarr[0].legend()
-		axarr[1].legend()
-		axarr[2].legend()
-
 def main():
 	T = 100
 	eta = 0.1
@@ -150,10 +138,20 @@ def main():
 	WMA, RWMA
 	stochastic,deterministic,adversarial
 	"""
-	WMA = Learner("WMA","deterministic", experts)
-	WMA.learn(T,eta)
-	WMA.plotLossGraph(T)
-	WMA.plotRegretGraph(T)
+	param1 = ["WMA","RWMA"]
+	param2 = ["stochastic","deterministic","adversarial"]
+	"""
+	for i in param1:
+		for j in param2:
+			predict = Learner(i,j, experts)
+			predict.learn(T,eta)
+			predict.plotLossGraph(T)
+			predict.plotRegretGraph(T)"""
+	predict = Learner("WMA","stochastic", experts)
+	predict.learn(T,eta)
+	predict.plotLossGraph(T)
+	predict.plotRegretGraph(T)
+
 	plt.show()
 
 if __name__ == "__main__":
